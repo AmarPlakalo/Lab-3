7017374a0d05997ac216091bfcbbba1f40258973
AmarPlakalo <114601596+AmarPlakalo@users.noreply.github.com>
2022-11-14 18:56:36 +0000

    Replaced onclicks with rxjs
    I did the rxjs part for adding a note, editing a note and deleting a note using fromEvent function that detects when each of the buttons are clicked.

--- Problem 1/script.js
+++ Problem 1/script.js
@@ -3,6 +3,11 @@ function button_action()
 	let title_content = document.getElementById('title_note').value;
 	let note_content = document.getElementById('yournote').value;
 	let colour_content = document.getElementById('colours').value;
+	let add_button = document.getElementById('add_note_btn');
+	
+	rxjs.fromEvent(add_button, 'click')
+		.subscribe(() => add_note_function()
+	);
 	
 	const node = document.createElement("div");
 	
@@ -13,7 +18,12 @@ function button_action()
 	input.className = "button";
 	input.value = "Edit Note";
 	
-	input.onclick = function () {
+	rxjs.fromEvent(input, 'click')
+	  .subscribe(() => edit_note_function()
+	);
+	
+	function edit_note_function()
+	{
 		var title = window.prompt("Enter new title : ");
 			
 		var content = window.prompt("Enter new content : ");
@@ -22,23 +32,30 @@ function button_action()
 		node.appendChild(document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ));
 		node.appendChild(input);
 		node.appendChild(input_2);
-		
-	};
-
+			
+	}
 	
 	var input_2 = document.createElement("input");
 	input_2.type = "button";
 	input_2.className = "button";
 	input_2.value = "Delete Note";
-	input_2.onclick = function () {
-		node.remove();
-	};
+	rxjs.fromEvent(input_2, 'click')
+	  .subscribe(() => delete_note_function()
+	);
 	
-	node.innerHTML = title_content + "<br><br><br>" + note_content;
-	node.appendChild(document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ));
-	node.appendChild(input);
-	node.appendChild(input_2);
+	function delete_note_function()
+	{
+		node.remove();
+	}
+	
+	function add_note_function()
+	{
+		node.innerHTML = title_content + "<br><br><br>" + note_content;
+		node.appendChild(document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ));
+		node.appendChild(input);
+		node.appendChild(input_2);
 
+	}
 	document.getElementById("DisplayNote").appendChild(node);
 	
 }
