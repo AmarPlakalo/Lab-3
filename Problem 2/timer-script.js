c1c01e092e2dda89a56369340c7c8ef02fe7334c
AmarPlakalo <114601596+AmarPlakalo@users.noreply.github.com>
2022-11-14 12:37:25 +0000

    Added javascript file with a few lines of code
    Just taking the values from the hours, minutes and seconds input fields and putting them inside a variable.

---
+++ Problem 2/timer-script.js
@@ -0,0 +1,6 @@
+function start_timer()
+{
+	let hours = document.getElementById("hours").value;
+	let minutes = document.getElementById("minutes").value;
+	let seconds = document.getElementById("seconds").value;
+}
